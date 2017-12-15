6e86d4bcd2b255780c8ae2b05707a786f6fc3f06
Valdeci <valdecipti@gmail.com>
2017-12-15 00:55:27 -0200

    Adicionado recurso para disparar um serviço que executa uma determinada rotina quando uma parte do widget é clicado.

---
+++ app/src/main/java/com/example/android/mygarden/PlantWateringService.java
@@ -0,0 +1,60 @@
+package com.example.android.mygarden;
+
+import android.app.IntentService;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.example.android.mygarden.provider.PlantContract;
+import com.example.android.mygarden.utils.PlantUtils;
+
+import static com.example.android.mygarden.provider.PlantContract.BASE_CONTENT_URI;
+import static com.example.android.mygarden.provider.PlantContract.PATH_PLANTS;
+
+/**
+ * Created by gemeos_valdeci on 15/12/2017.
+ */
+
+public class PlantWateringService extends IntentService {
+    public static final String LOG = PlantWateringService.class.getSimpleName();
+
+    public static final String ACTION_WATER_PLANTS =
+            "com.example.android.mygarden.action.water_plants";
+
+    public PlantWateringService() {
+        super("PlantWateringService");
+    }
+
+    public static void startActionWaterPlants(Context context) {
+        Intent intent = new Intent(context, PlantWateringService.class);
+        intent.setAction(ACTION_WATER_PLANTS);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_WATER_PLANTS.equals(action)) {
+                handleActionWaterPlants();
+            }
+        }
+    }
+
+    private void handleActionWaterPlants() {
+        Uri PLANTS_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_PLANTS).build();
+        ContentValues contentValues = new ContentValues();
+        long timeNow = System.currentTimeMillis();
+        Log.i(LOG, "handle Action Water plants");
+        contentValues.put(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME, timeNow);
+        // Atualiza apenas plantas que ainda estão vivas
+        getContentResolver().update(
+                PLANTS_URI,
+                contentValues,
+                PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME+">?",
+                new String[]{String.valueOf(timeNow - PlantUtils.MAX_AGE_WITHOUT_WATER)});
+    }
+}
